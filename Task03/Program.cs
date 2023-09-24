5a02b95a27ee67593f703ca36c9a4cd2c5be00ca
Arghara <arghara@gmail.com>
2023-09-24 14:26:25 +0300

    Решение для третьей задачи

---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿﻿Console.Write("Введите цифру, обозначающую день недели: "); // Ввод значения пользователем
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayWeek == 6 || dayWeek == 7)
+{
+    Console.WriteLine("{0} -> Ура! Этот день выходной", dayWeek);
+}
+else if (dayWeek >= 1 && dayWeek <= 5)
+{
+    Console.WriteLine("{0} -> Увы, этот день не выходной", dayWeek);
+}
+else
+{
+    Console.WriteLine("{0} -> Дня недели с такой цифрой нет", dayWeek);
+}
