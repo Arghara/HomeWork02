95df9af7439aa04a6ff7499e022cf963caacac51
Arghara <arghara@gmail.com>
2023-09-24 14:11:30 +0300

    Решение первой задачи

---
+++ Task01/Program.cs
@@ -0,0 +1,8 @@
+﻿﻿Console.Write("Введите трехзначное число: "); // Ввод значения пользователем 
+int number = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = (number % 100) / 10;
+
+Console.Write("Итак, вторая цифра заданного числа - это ");
+
+Console.WriteLine(secondNumber);
